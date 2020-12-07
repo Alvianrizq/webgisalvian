64459e1e44eb2ac40505567d702526b358f0d08d
Alvianrizq <75605714+Alvianrizq@users.noreply.github.com>
2020-12-07 14:34:54 +0700

    Add files via upload

---
+++ data/webgis_1.js
@@ -0,0 +1,8 @@
+var json_webgis_1 = {
+"type": "FeatureCollection",
+"name": "webgis_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 110.832756025414952, -6.823483051069494 ] } }
+]
+}
